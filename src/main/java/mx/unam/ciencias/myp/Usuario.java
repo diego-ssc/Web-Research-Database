2a117d2e831672b430755aa4b4e5cdae3f4f4cba
diego-ssc <diego_ssc@ciencias.unam.mx>
2022-11-19 21:55:52 -0600

    database-table

--- src/main/java/mx/unam/ciencias/myp/Usuario.java
+++ src/main/java/mx/unam/ciencias/myp/Usuario.java
@@ -1,9 +1,14 @@
 package mx.unam.ciencias.myp;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
 
     private String nombre;
