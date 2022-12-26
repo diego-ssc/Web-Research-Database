8c873ce6833fb3a9a529c4ce11ae432e55c0a35f
José Alberto Rosales Peña <joserosales@ciencias.unam.mx>
2022-12-26 05:42:11 -0600

    PruebasUnitarias

--- src/test/java/mx/unam/ciencias/myp/MypApplicationTests.java
+++ src/test/java/mx/unam/ciencias/myp/MypApplicationTests.java
@@ -3,9 +3,27 @@ package mx.unam.ciencias.myp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = MypApplication.class)
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+
+@DataJpaTest
 class MypApplicationTests {
     @Test
     void contextLoads() {
     }
+
+    @Autowired
+    RepositorioUsuario repositorio;
+    Usuario usuario = new Usuario();
+
+    @Test
+    void testSave() {
+
+        Iterable<Usuario> i = repositorio.findAll();
+        for (Usuario u : i) {
+        }
+    }
+
+
 }
